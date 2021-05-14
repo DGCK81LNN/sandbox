fe4001c2619f083edb850cbd3ca888be4dc8cfb2
DGCK81LNN <triluolnn@163.com>
2021-05-14 23:57:35 +0800

    feat(xml-richard): 剧情文件新语法
    - 不再允许 case 元素中出现 else 元素；
    - 添加 if、elif 元素，与 else 配合使用作为更简易的流程控制；
    - 现在如果 case 的所有 when 都不符合条件，程序会报错；
    - 添加 s 元素，可以在输出中添加行内元素 span、b、i、u、em 或 strong 并设置 lang、class、style 属性；
    - 添加 call 元素，目前尚未实现，将来可能用于实现剧情扩展包。

--- xml-richard/index.js
+++ xml-richard/index.js
@@ -8,15 +8,22 @@ xhr.send();
 var outEl;
 /** @type {XMLDocument} */
 var doc;
+/**
+ * @typedef {{
+ *   epidemicRecord: number[],
+ *   volumesUnlocked: boolean,
+ *   restoreVolume: string,
+ *   restoreData: number[],
+ * }} Data
+ * @type {Data}
+ */
 var data = {
-  /** @type {(-1 | 0 | 1)[]} */
   epidemicRecord: [],
   volumesUnlocked: false,
   restoreVolume: "",
-  /** @type {number[]} */
   restoreData: [],
 };
-/** 内嵌Js的变量 */
+/** 内嵌Js的变量“作用域” */
 var vars = { data };
 /** @type {Element} */
 var volume;
@@ -24,7 +31,7 @@ var restoreIndex = 0;
 
 try {
   Object.assign(data, JSON.parse(localStorage["soulSandboxLoveWithRichardData"]));
-} catch (_) { console.log(_) }
+} catch (_) { console.log("恢复数据失败，已忽略\n", _) }
 
 function init() {
   outEl = document.getElementById("main");
@@ -46,7 +53,6 @@ function init() {
 
 /**
  * 运行richard剧情文件
- * 
  * @param {string} volumeId 要运行的`volume`的`id`
  * @param {number[]} restoreData 恢复之前进度
  */
@@ -59,14 +65,16 @@ async function run(volumeId, restoreData = []) {
     volume = doc.getElementById(volumeId);
 
     while (outEl.firstChild) outEl.removeChild(outEl.firstChild);
-    outEl.appendChild(document.createElement("h1"))
-      .textContent = doc.evaluate("title", volume).iterateNext().textContent;
+    const titleEl = doc.evaluate("title", volume).iterateNext();
+    outEl.appendChild(importContent(titleEl, "h1"));
 
     await iterate(volume);
 
     data.restoreVolume = "";
     data.restoreData = [];
     save();
+
+    location.reload();
   } catch (err) {
     outEl.appendChild(document.createElement("p"))
       .appendChild(document.createElement("i"))
@@ -77,50 +85,67 @@ async function run(volumeId, restoreData = []) {
 
 /**
  * 运行一个`richard`节点
- * 
  * @param {Element} element 
  */
 async function iterate(element) {
-  for (let child of element.children) {
-    if (child.nodeName === "p") {
-      outEl.appendChild(document.createElement("p"))
-        .textContent = child.textContent;
-    } else if (child.nodeName === "h") {
-      outEl.appendChild(document.createElement("h2"))
-        .textContent = child.textContent;
-    } else if (child.nodeName === "i") {
-      outEl.appendChild(document.createElement("p"))
-        .appendChild(document.createElement("i"))
-        .textContent = child.textContent;
-    } else if (child.nodeName === "a") {
-      await execJs(child.getAttribute("js"));
-    } else if (child.nodeName === "js") {
-      await execJs(child.textContent);
-    } else if (child.nodeName === "separator") {
-      outEl.appendChild(document.createElement("hr"));
-    } else if (child.nodeName === "choose") {
-      let choices = [];
-      for (let el of child.children) {
-        if (el.nodeName === "choice") {
-          choices.push(el.textContent);
+  var skipElseBlocks = false;
+  childLoop: for (let child of element.children) {
+    switch (child.nodeName) {
+      case "p":
+        outEl.appendChild(importContent(child, "p"));
+        break;
+      case "h":
+        outEl.appendChild(importContent(child, "h2"));
+      case "i":
+        outEl.appendChild(document.createElement("p"))
+          .appendChild(importContent(child, "i"));
+        break;
+      case "a":
+        await execJs(child.getAttribute("js"));
+        break;
+      case "js":
+        await execJs(child.textContent);
+        break;
+      case "separator":
+        outEl.appendChild(document.createElement("hr"));
+        break;
+      case "pause":
+        await pause();
+        break;
+      case "choose":
+        vars._ = await choose(child);
+        break;
+      case "if":
+        if (skipElseBlocks = await execJs(child.getAttribute("js")))
+          await iterate(child);
+        break;
+      case "elif":
+        if (!skipElseBlocks && (skipElseBlocks = await execJs(child.getAttribute("js"))))
+          await iterate(child);
+        break;
+      case "else":
+        if (!skipElseBlocks)
+          await iterate(child);
+        break;
+      case "case":
+        for (let when of child.children) {
+          if (when.nodeName === "else" ||
+            when.nodeName === "when" && await execJs(when.getAttribute("js"))) {
+            await iterate(when);
+            continue childLoop;
+          }
         }
-      }
-      vars._ = await choose(choices);
-    } else if (child.nodeName === "case") {
-      for (let el of child.children) {
-        if (el.nodeName === "else" ||
-          el.nodeName === "when" && await execJs(el.getAttribute("js"))) {
-          await iterate(el);
-          break;
-        }
-      }
+        // 没有找到匹配的<when>，抛出错误
+        console.error("iterate(): <case>块中所有<when>都不匹配，<case>元素：\n", child);
+        throw new Error("iterate(): <case>块中所有<when>都不匹配");
+      case "call":
+      // TODO: Implement expansive modules
     }
   }
 }
 
 /**
  * 运行JavaScript代码片段
- * 
  * @param {string} js 
  * @returns {*} 运行结果
  */
@@ -142,8 +167,7 @@ function execJs(js) {
 var chooseIndex = 1;
 /**
  * 显示互动选择
- * 
- * @param {string[]} choices 选项列表
+ * @param {Element} choices 选项列表
  * @returns {Promise<number>} 所选选项索引
  */
 function choose(choices) {
@@ -163,7 +187,7 @@ function choose(choices) {
     chooseEl.disabled = true;
   }
 
-  choices.forEach((item, index) => {
+  [...choices.children].forEach((item, index) => {
     const itemId = `${choicesName}-choice${index}`;
     const itemEl = choicesEl.appendChild(document.createElement("div"));
     const radio = itemEl.appendChild(document.createElement("input"));
@@ -175,10 +199,10 @@ function choose(choices) {
       chosenIndex = index;
       button.disabled = false;
     });
-    const label = itemEl.appendChild(document.createElement("label"));
+
+    const label = itemEl.appendChild(importContent(item, "label"));
     label.setAttribute("for", itemId);
     label.htmlFor = itemId;
-    label.textContent = item;
   });
 
   if (willRestore) return Promise.resolve(chosenIndex);
@@ -199,10 +223,56 @@ function choose(choices) {
 }
 
 /**
+ * 显示“继续”按钮
+ * @returns {Promise<void>}
+ */
+function pause() {
+  outEl.appendChild(document.createElement("hr"));
+
+  if (restoreIndex < data.restoreData.length) {
+    restoreIndex++;
+    return Promise.resolve();
+  }
+
+  const pauseEl = outEl.appendChild(document.createElement("div"));
+  const button = pauseEl.appendChild(document.createElement("button"));
+  button.className = "pause";
+  button.textContent = "继续";
+  return new Promise(resolve => {
+    button.onclick = function () {
+      pauseEl.remove();
+      data.restoreData.push(0);
+      restoreIndex++;
+      save();
+      resolve();
+    }
+  });
+}
+
+/**
  * 保存`data`
  */
 function save() {
   try {
     localStorage["soulSandboxLoveWithRichardData"] = JSON.stringify(data);
   } catch (_) { }
+}
+
+/**
+ * 导入一个元素内的文本
+ * @param {Element} origNode
+ * @param {string} destNodeName
+ */
+function importContent(origNode, destNodeName) {
+  var destNode = document.createElement(destNodeName);
+  if (origNode.hasAttribute("lang")) destNode.setAttribute("lang", origNode.getAttribute("lang"));
+  if (origNode.hasAttribute("class")) destNode.setAttribute("class", origNode.getAttribute("class"));
+  if (origNode.hasAttribute("style")) destNode.setAttribute("style", origNode.getAttribute("style"));
+  for (let child of origNode.childNodes) {
+    if (child.nodeType === Node.TEXT_NODE)
+      destNode.appendChild(document.importNode(child));
+    else
+      destNode.appendChild(importContent(child, child.getAttribute("is") || "span"));
+  }
+  return destNode;
 }
